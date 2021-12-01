525648b3f1118f7657fe3307ec01da627c049179
ohdaesu2 <90598930+ohdaesu2@users.noreply.github.com>
2021-12-01 16:21:15 +0900

    Develop merge (#2)
    * feat: Register PostAdmin
    
    * feat: Add list_display into PostAdmin

--- askcompany/instagram/admin.py
+++ askcompany/instagram/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from askcompany.instagram.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'message', 'is_public', )
