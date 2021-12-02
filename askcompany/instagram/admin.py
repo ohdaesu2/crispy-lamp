310d04c7c3e591006c1ebe6cf0e62a1e2f108b89
ohdaesu2 <eotn4728@humelo.com>
2021-12-02 16:51:24 +0900

    create_instagram_admin

--- askcompany/instagram/admin.py
+++ askcompany/instagram/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from askcompany.instagram.models import Post
 
 
-@admin.register(Post)
+@admin.register()
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('author', 'message', 'is_public', )
+    list_display = ['author', 'message', 'is_public']
+    list_display_links = ['message']
